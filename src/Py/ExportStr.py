e3bdacf7335dc9fa58887251b064f17120adfdc1
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2018-05-13 21:50:29 +0900

    updated ExportStr

--- src/Py/ExportStr.py
+++ src/Py/ExportStr.py
@@ -14,14 +14,17 @@ def main():
         istart += 1
 
     if len(sys.argv) < istart + 3:
-        print('%s [--cp=<codepage>] binfile start end [output file]' % sys.argv[0])
+        print('%s [--cp=<codepage>] binfile start end [base] [output file]' % sys.argv[0])
+        print('  default base = 0')
         print('  default output file = binfile.txt')
         return
 
     filein = sys.argv[istart + 0];
-    fileout = filein + '.txt' if len(sys.argv) == istart + 3 else sys.argv[istart + 3]
+    fileout = filein + '.txt' if len(sys.argv) <= istart + 4 else sys.argv[istart + 4]
     start = int(sys.argv[istart + 1], 16) if sys.argv[istart + 1].lower().startswith('0x') else int(sys.argv[istart + 1])
     end = int(sys.argv[istart + 2], 16) if sys.argv[istart + 2].lower().startswith('0x') else int(sys.argv[istart + 2])
+    base = '0' if len(sys.argv) <= istart + 3 else sys.argv[istart + 3]
+    base = int(base, 16) if base.lower().startswith('0x') else int(base)
 
     fs = open(filein, 'rb')
     buff = fs.read()
@@ -34,7 +37,10 @@ def main():
     fs_out = open(fileout, mode='w', encoding=CODE_PAGE)
     for s in strs:
         if i % 4 == 0:
-            fs_out.write('%08X,%s\n' % (i, str(s, encoding=CODE_PAGE).replace('\n', "\\n")))
+            fs_out.write('%08X\n' % (i + base))
+            fs_out.write('.%s\n' % CODE_PAGE)
+            fs_out.write(str(s, encoding=CODE_PAGE))
+            fs_out.write('\n')
         i += len(s) + 1
     fs_out.close()
 
